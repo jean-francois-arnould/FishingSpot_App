a8a5ad50ee7d127030078fb0dc4e19eac4a4c0c0
Jean-Francois Arnould <Jean-Francois.Arnould@lu.ey.com>
2026-03-20 10:52:58 +0100

    manual push

--- Models/FishingBrand.cs
+++ Models/FishingBrand.cs
@@ -1,7 +1,7 @@
 using System.Text.Json.Serialization;
 
-namespace FishingSpot.PWA.Models
-{
+namespace FishingSpot.PWA.Models 
+{ 
     public class FishingBrand
     {
         [JsonPropertyName("id")]
